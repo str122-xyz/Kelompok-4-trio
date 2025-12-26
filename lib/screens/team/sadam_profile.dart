19832681cd317f9837f8b088de01da8fa2db912d
SadamIRM <sadamirmar@gmail.com>
2025-12-26 23:45:22 +0700

    Scaffold Awal

---
+++ lib/screens/team/sadam_profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SadamProfile extends StatefulWidget {
+  const SadamProfile({super.key});
+
+  @override
+  State<SadamProfile> createState() => _SadamProfileState();
+}
+
+class _SadamProfileState extends State<SadamProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff284B63),
+    );
+  }
+}
