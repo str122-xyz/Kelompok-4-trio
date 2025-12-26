e09063d58e3c6b4eaac4bc91a9184b3945754f61
ENZHOUHUY <1123150029@global.ac.id>
2025-12-26 15:23:36 +0700

    membuat statefull splash screen enzho

---
+++ lib/screens/splash/splash_screen_3.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SplashEnzho extends StatefulWidget {
+  const SplashEnzho({super.key});
+
+  @override
+  State<SplashEnzho> createState() => _SplashEnzhoState();
+}
+
+class _SplashEnzhoState extends State<SplashEnzho> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+            children: [],
+        ),
+      ),
+    );
+  }
+}
