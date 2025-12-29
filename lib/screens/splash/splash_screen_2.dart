59497d27f7dfd358d93c50f2b1f45092e44aa2cd
ZEINadli <1123150028@global.ac.id>
2025-12-30 00:46:51 +0700

    circel border ke tengah warna transparan

--- lib/screens/splash/splash_screen_2.dart
+++ lib/screens/splash/splash_screen_2.dart
@@ -7,7 +7,20 @@ class SplashZeinn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFA7CCED),
-      body: SafeArea(child: Column(children: [const Spacer()])),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Spacer(),
+
+            Center(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(customBorder: const CircleBorder()),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
