60cc7f81b999c731e94b5216a1aca1972989d50e
ZEINadli <1123150028@global.ac.id>
2025-12-30 00:29:58 +0700

    stateless widget(stl tab)

---
+++ lib/screens/splash/splash_screen_2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashZeinn extends StatelessWidget {
+  const SplashZeinn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
