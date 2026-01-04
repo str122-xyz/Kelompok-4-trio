6f72e0ca6a7e6901eac85694545506a6f114c8c4
Satriahrl_ <satriaherlambang22@gmail.com>
2026-01-04 22:46:45 +0700

    routing

--- lib/screens/splash/splash_screen_4.dart
+++ lib/screens/splash/splash_screen_4.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:kelompok4_app_mobile/screens/auth/login_page.dart';
-import 'package:kelompok4_app_mobile/screens/dashboard/dashboard.dart';
-import 'package:kelompok4_app_mobile/screens/team/felan_profile.dart';
-import 'package:kelompok4_app_mobile/screens/team/sadam_profile.dart';
-import 'package:kelompok4_app_mobile/screens/team/satria_profile.dart';
-import 'package:kelompok4_app_mobile/screens/team/zeinn_profile.dart';
 
 class SplashScreenSadam extends StatefulWidget {
   const SplashScreenSadam({super.key});
@@ -49,7 +44,7 @@ class _SplashScreenSadamState extends State<SplashScreenSadam> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => const SadamProfile(),
+                        builder: (context) => const LoginPage(),
                       ),
                     );
                   },
