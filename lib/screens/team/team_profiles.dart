a6f0c190c32e52088fb44266d595c7ede9159ebd
Felarrn <felanardentayogaadiyatama@gmail.com>
2026-01-05 05:40:47 +0700

    membuat stateful widget untuk team profile

---
+++ lib/screens/team/team_profiles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ProfileTeam extends StatefulWidget {
+  const ProfileTeam({super.key});
+
+  @override
+  State<ProfileTeam> createState() => _ProfileTeamState();
+}
+
+class _ProfileTeamState extends State<ProfileTeam> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+            children: [],
+        ),
+      ),
+    );
+  }
+}
