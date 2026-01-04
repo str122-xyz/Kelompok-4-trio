d9e8d7f62aa4f2369cc9ae7222aabe9a762ba8cd
Satriahrl_ <satriaherlambang22@gmail.com>
2026-01-05 02:54:54 +0700

    feat: class can be moved using the mouse

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 import 'package:firebase_core/firebase_core.dart'; //Important
 import 'package:kelompok4_app_mobile/screens/splash/splash_screen1.dart';
 import 'firebase_options.dart';
@@ -10,12 +11,22 @@ void main() async {
   runApp(const MyApp());
 }
 
+// Class buat aktifin Mouse Drag
+class AppScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+    PointerDeviceKind.touch,
+    PointerDeviceKind.mouse,
+  };
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scrollBehavior: AppScrollBehavior(),
       debugShowCheckedModeBanner: false,
       title: 'Project UAS Kelompok 4 Trio',
       theme: ThemeData(useMaterial3: true),
