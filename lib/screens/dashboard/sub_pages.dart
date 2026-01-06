c4f550ee8b22a5c90fe44782256721ab2b1c3c5b
Satriahrl_ <satriaherlambang22@gmail.com>
2026-01-06 23:43:05 +0700

    new class stl initial scaffold

--- lib/screens/dashboard/sub_pages.dart
+++ lib/screens/dashboard/sub_pages.dart
@@ -128,3 +128,27 @@ class JadwalUasPage extends StatelessWidget {
     );
   }
 }
+
+class SusulanPages extends StatelessWidget {
+  const SusulanPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          "Info Ujian Susulan",
+          style: TextStyle(
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+    );
+  }
+}
