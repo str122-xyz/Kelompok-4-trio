41f37a0f2322090a5bf3e8c0c3f50f54b983fa91
Satriahrl_ <satriaherlambang22@gmail.com>
2026-01-08 01:44:00 +0700

    body notif

--- lib/screens/dashboard/notif_page.dart
+++ lib/screens/dashboard/notif_page.dart
@@ -15,6 +15,17 @@ class NotifPage extends StatelessWidget {
         foregroundColor: Colors.black,
         elevation: 0,
       ),
+      body: notification.isEmpty
+      ? Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.notifications_off_outlined, size: 80, color: Colors.grey[300]),
+            const SizedBox(height: 20),
+            const Text("Belum ada notifikasi baru", style: TextStyle(color: Colors.grey)),
+          ],
+        ),
+      ),
     );
   }
 }
